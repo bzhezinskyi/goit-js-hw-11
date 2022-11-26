ea57e53255ebc44949d9e99ef866bfb69506f027
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-11-26 13:13:03 +0200

    updata page

--- src/js/crieateNewGallery.js
+++ src/js/crieateNewGallery.js
@@ -1,4 +1,6 @@
 import fetchPhoto from './fetchPhoto';
+import crieateGallery from './crieateGallery';
+import crieatePage from './crieatePage';
 import Notiflix from 'notiflix';
 
 const refs = {
@@ -7,7 +9,7 @@ const refs = {
 };
 
 export default function crieateNewGallery(query) {
-  fetchPhoto(query, 1)
+  fetchPhoto(query, crieatePage(1))
     .then(arrey => {
       const element = document.getElementById('content');
       element.scrollIntoView();
@@ -25,34 +27,8 @@ export default function crieateNewGallery(query) {
             arrey.data.totalHits - refs.gallery.children.length
           } images.`
         );
+        refs.loadMore.classList.remove('visually-hidden');
       }
     })
     .catch(error => console.log(Notiflix.Notify.failure(`${error.message}`)));
-}
-
-function crieateGallery(photos) {
-  for (const photo of photos) {
-    refs.gallery.innerHTML += `<div class="photo-card">
-  <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      <b>${photo.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      <b>${photo.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      <b>${photo.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      <b>${photo.downloads}</b>
-    </p>
-  </div>
-</div>`;
-  }
-  refs.loadMore.classList.remove('visually-hidden');
 }
--- src/js/crieateNextGallery.js
+++ src/js/crieateNextGallery.js
@@ -1,54 +1,26 @@
 import fetchPhoto from './fetchPhoto';
+import crieateGallery from './crieateGallery';
+import crieatePage from './crieatePage';
 import Notiflix from 'notiflix';
 
 const refs = {
   gallery: document.querySelector('.gallery'),
   loadMore: document.querySelector('.load-more'),
 };
-let page = 2;
-
 export default function crieateNextGallery(query) {
-  fetchPhoto(query, page)
-    .then(arrey => {
-      if (arrey.data.totalHits === refs.gallery.children.length) {
-        endGallery();
-      } else {
-      }
+  fetchPhoto(query, crieatePage(2)).then(arrey => {
+    if (arrey.data.totalHits === refs.gallery.children.length) {
+      endGallery();
+    } else {
       crieateGallery(arrey.data.hits);
       Notiflix.Notify.info(
         `Hooray! We found ${
           arrey.data.totalHits - refs.gallery.children.length
         } images.`
       );
-    })
-    .catch(error => console.log(Notiflix.Notify.failure(`${error.message}`)));
-}
-
-function crieateGallery(photos) {
-  for (const photo of photos) {
-    refs.gallery.innerHTML += `<div class="photo-card">
-  <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      <b>${photo.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      <b>${photo.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      <b>${photo.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      <b>${photo.downloads}</b>
-    </p>
-  </div>
-</div>`;
-  }
-  page += 1;
+      page += 1;
+    }
+  });
 }
 
 function endGallery() {
